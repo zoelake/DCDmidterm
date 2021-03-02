cdea229a892b94afa5a4f971c9af216a91fc43ed
Eilidh Morrice-Ashdown <eilidhma@gmail.com>
2021-03-01 22:45:37 -0800

    swipe up component active, but only onclick

--- comps/swipe/index.js
+++ comps/swipe/index.js
@@ -1,12 +1,17 @@
 function SwipeUI() {
   return `
   <div style="
-  height: 200px;
-  width: 200px;
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-">
+  margin-top: 50px;
+  margin-bottom: 50px;
+  transition: opacity 1s, margin-top 1s;
+  " 
+  onclick='SwipeUI.HandleSwipe(this)'
+  >
   <p style="
   font-family: 'Montserrat', sans-serif;
   font-size: 36px;
@@ -14,12 +19,19 @@ function SwipeUI() {
   "
   >SWIPE UP</p>
   <img style="
-  width: 30%;
-  height: 30%;
+  width: 10%;
+  height: 10%;
   "
   src="./images/arrow.svg" alt="arrow">
 </div>
   `
+}
+
+SwipeUI.HandleSwipe = (el) => {
+
+  el.style.opacity = "0"
+  el.style.marginTop = "-100px";
+
 }
 
 // export const SwipeButton = SwipeUI();
