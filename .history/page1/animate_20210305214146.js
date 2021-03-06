6feb69bca7f9b6a9a07d13268c92e16102cf3283
zoelake <zoe_lake@icloud.com>
2021-03-05 21:54:56 -0800

    first draft js for page 1

---
+++ .history/page1/animate_20210305214146.js
@@ -0,0 +1,19 @@
+var lettuce = document.querySelector("#lettuce-img");
+imgTimer = null;
+
+function HoverImgUp() {
+    lettuce.style.bottom = "15px";
+}
+
+// function HoverImgDown() {
+//     lettuce.style.bottom = "0px";
+// }
+
+lettuce.onmouseover = function () {
+    imgTimer = setInterval(HoverImgUp, 200)
+}
+
+lettuce.onmouseout = function () {
+    clearInterval(imgTimer)
+    imgTimer = null
+}
---
+++ .history/page1/animate_20210305214248.js
@@ -0,0 +1,9 @@
+var lettuce = document.querySelector("#lettuce-img");
+
+function HoverImgUp() {
+    el.style.bottom = "15px";
+}
+
+function HoverImgDown() {
+    el.style.bottom = "0px";
+}
